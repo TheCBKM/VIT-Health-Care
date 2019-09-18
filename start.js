38e7fce974f6c6d103e09281a273b62567ff24de
cbkm-company <you@example.com>
2019-09-19 04:02:10 +0530

    first commit

--- start.js
+++ start.js
@@ -1,21 +1,21 @@
-const dementia=require('./questions/demetia.json')
-const depress=require('./questions/depress.json')
-const bt=require('./questions/brain-teasers.json')
-const app= require('express')();
+const dementia = require('./questions/demetia.json')
+const depress = require('./questions/depress.json')
+const bt = require('./questions/brain-teasers.json')
+const app = require('express')();
 
 
-console.log(dementia)
-console.log(depress)
-console.log(bt)
+// console.log(dementia)
+// console.log(depress)
+// console.log(bt)
 
-app.get('/dementia',(req,res)=>{
+app.get('/dementia', (req, res) => {
     res.json(dementia)
 })
-app.get('/depress',(req,res)=>{
+app.get('/depress', (req, res) => {
     res.json(dementia)
 })
-app.get('/bt',(req,res)=>{
+app.get('/bt', (req, res) => {
     res.json(bt)
 })
 
-app.listen(process.env.PORT||5000)
+app.listen(process.env.PORT || 5000)
