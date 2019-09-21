12cf2527f73520a87cc9aa563c5539a397a12894
cbkm-company <you@example.com>
2019-09-21 17:30:45 +0530

    hindi

--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -16,6 +16,28 @@ app.get('/getall', (req, res) => {
     })();
 })
 
+app.post('/login', (req, res) => {
+    (async () => {
+        try {
+            console.log(req.body)
+            Patient.findOne({ 'phone': req.body.phone }, async (err, user) => {
+                if (!user) {
+                    contactPromise = await patientServices.savePatient(req.body);
+                    res.json({ success: true, data: contactPromise })
+                }
+                else if (err) return res.json({ loginSuccess: false, message: 'Auth failedd', err });
+                else {
+                    res.json({ success: true, data: user })
+                }
+            })
+        }
+        catch (e) {
+            console.log(e)
+            res.json({ success: false })
+        }
+    })();
+})
+
 
 app.post('/save', (req, res) => {
     (async () => {
@@ -33,16 +55,8 @@ app.post('/save', (req, res) => {
 app.post('/addrecord', (req, res) => {
     (async () => {
         try {
-            Patient.findOne({ 'phone': req.body.phone }, async(err, user) => {
-                if (!user) return res.json({ success: true, message: 'Auth failed, phone not found' });
-                else if (err) return res.json({ loginSuccess: false, message: 'Auth failed', err });
-                else {
-
-                    contactPromise = await patientServices.addRecord(req.body);
-                    res.json({ success: true, data: contactPromise })
-                }
-
-            })
+            contactPromise = await patientServices.addRecord(req.body);
+            res.json({ success: true, data: contactPromise })
         }
         catch (e) {
             console.log(e)
